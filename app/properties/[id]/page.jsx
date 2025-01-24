d68a28fee7182f0164b4ac89f4af1b24043c329e
Eduardo Orozco <eddthedev@Eddthedevs-MacBook-Pro.local>
2025-01-24 16:47:48 -0600

    Using usePathname

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,16 +1,19 @@
 'use client';
-import { useRouter, useParams, useSearchParams } from "next/navigation";
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
 
 const PropertyPage = () => {
     const router = useRouter()
     const params = useParams()
     const searchParams = useSearchParams()
+    const pathname = usePathname()
+
     console.log(router)
     return (
         <div>
             {/* <button onClick={() => router.replace('/')}>Home</button> */}
             {/* Property Page {params.id} */}
-            Property Page { searchParams.get('name') }
+            {/* Property Page { searchParams.get('name') } */}
+            Property Page {pathname}
         </div>
     );
 };
