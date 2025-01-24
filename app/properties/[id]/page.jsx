8b88dda31d3900380df52d8864b75d689e38c558
Eduardo Orozco <eddthedev@Eddthedevs-MacBook-Pro.local>
2025-01-24 16:44:35 -0600

    Using useSearchParams

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,14 +1,16 @@
 'use client';
-import { useRouter, useParams } from "next/navigation";
+import { useRouter, useParams, useSearchParams } from "next/navigation";
 
 const PropertyPage = () => {
     const router = useRouter()
     const params = useParams()
+    const searchParams = useSearchParams()
     console.log(router)
     return (
         <div>
             {/* <button onClick={() => router.replace('/')}>Home</button> */}
-            Property Page {params.id}
+            {/* Property Page {params.id} */}
+            Property Page { searchParams.get('name') }
         </div>
     );
 };
