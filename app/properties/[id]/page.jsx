4174f2d8a52d4658face23b6c67db0966caadaaf
Eduardo Orozco <eddthedev@Eddthedevs-MacBook-Pro.local>
2025-01-24 16:53:35 -0600

    Using Server side params demo

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,19 +1,8 @@
-'use client';
-import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
+const PropertyPage = ({params}) => {
 
-const PropertyPage = () => {
-    const router = useRouter()
-    const params = useParams()
-    const searchParams = useSearchParams()
-    const pathname = usePathname()
-
-    console.log(router)
     return (
         <div>
-            {/* <button onClick={() => router.replace('/')}>Home</button> */}
-            {/* Property Page {params.id} */}
-            {/* Property Page { searchParams.get('name') } */}
-            Property Page {pathname}
+            Property Page {params.id} 
         </div>
     );
 };
